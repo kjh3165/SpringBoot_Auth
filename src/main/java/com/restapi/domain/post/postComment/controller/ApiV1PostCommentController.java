9bbb07560d4f0211f9b19785477359df8b6f41fb
kjh3165 <jhwk109@gmail.com>
2026-06-01 15:47:08 +0900

    댓글 등록 apiKey 인증방식으로 수정, 테스트 케이스 수정

--- src/main/java/com/restapi/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/restapi/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -8,10 +8,13 @@ import com.restapi.domain.post.postComment.dto.PostCommentDto;
 import com.restapi.domain.post.postComment.dto.PostCommentModifyReqBody;
 import com.restapi.domain.post.postComment.dto.PostCommentWriteReqBody;
 import com.restapi.domain.post.postComment.entity.PostComment;
+import com.restapi.global.exception.ServiceException;
 import com.restapi.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -76,10 +79,16 @@ public class ApiV1PostCommentController {
     @Operation(summary = "작성")
     public RsData<PostCommentDto> write(
             @PathVariable long postId,
-            @Valid @RequestBody PostCommentWriteReqBody reqBody
+            @Valid @RequestBody PostCommentWriteReqBody reqBody,
+            @NotBlank @Size(min = 2, max = 50) @RequestHeader("Authorization") String authorization
     ) {
+        String apiKey = authorization.replace("Bearer ", "");
+
+        Member author = memberService.findByApiKey(apiKey)
+                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
+
         Post post = postService.findById(postId);
-        Member author = memberService.findByUsername("user1").get();
+
         PostComment postComment = postService.writeComment(author, post, reqBody.content());
 
         // 트렌잭션 끝난 후 수행되야 하는 더티체킹 및 여가지 작업들을 지금 당장 수행시킴
