aba7f3200c7a00885d5d7fbe75bda6509a56380f
kjh3165 <jhwk109@gmail.com>
2026-06-05 09:50:38 +0900

    Ut.jwt.toString 함수를 이용해서 JWT 생성

--- src/test/java/com/restapi/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/restapi/domain/member/member/service/AuthTokenServiceTest.java
@@ -1,5 +1,6 @@
 package com.restapi.domain.member.member.service;
 
+import com.restapi.standard.util.Ut;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
@@ -52,6 +53,24 @@ public class AuthTokenServiceTest {
                 .expiration(expiration) // 만료날짜
                 .signWith(secretKey) // 키 서명
                 .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt : " + jwt);
+    }
+
+    @Test
+    @DisplayName("Ut.jwt.toString 통해서 jwt 생성, {name= \"Paul\", age=23}")
+    void t3 () {
+        String secret = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+        int expireSeconds = 60 * 60 * 24 * 365; // 토큰 만료시간을 1년
+        Map<String, Object> claims = Map.of("name", "David", "age", "20");
+
+        String jwt = Ut.jwt.toString(
+                secret,
+                expireSeconds,
+                claims
+        );
 
         assertThat(jwt).isNotBlank();
 
